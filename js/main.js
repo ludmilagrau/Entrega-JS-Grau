99ed3294ec886290dff190fe59dd6b002095bf31
ludmila.grau <ludmila.grau@wenance.com>
2023-05-03 21:40:54 -0300

    PreEntrega3Grau

--- js/main.js
+++ js/main.js
@@ -14,85 +14,124 @@ const calculoIntereses = (prestamo, cuotas) => {
     return valorCuota;
 }
 
- // Función que valida que el valor ingresado sea un número.
-const validacionNumber = (valorIngresado, mensaje) => {
-	while(isNaN(valorIngresado) || valorIngresado <= 0) {
-		valorIngresado = parseFloat(prompt(mensaje));
-	}
-	return valorIngresado
-}
-
-// Función que valida que la respuesta sea sí o no.
-const validacionRespuesta = (respuesta, mensaje) => {
-	while(respuesta != "si" && respuesta != "no") {
-		respuesta = prompt(mensaje);
-	}
-	return respuesta;
-}
-
 // Inicializamos el array que contendrá el historial de préstamos del usuario.
 let prestamos = [];
 
-// Mensaje de bienvenida.
-alert("¡Bienvenido! :) ¿Desea sacar un préstamo? Lo invitamos a simular una financiación.");
+// Obtenemos elementos del DOM.
+let formContainer = document.querySelector('.form__container')
+let resultsContainer = document.querySelector('.results__container')
+let rangoImporte = document.querySelector('#range');
+let importe = document.querySelector('#importe');
+let cuotas = document.querySelectorAll('.radio__input');
+let resetButton = document.querySelector('.reset__button');
+let historialButton = document.querySelector('.historial__button');
+let historialPrestamos = document.querySelector('.historial')
+let alerta = document.querySelector('.alert')
+let solicitarButton = document.querySelector(".solicitar__button")
 
-// Declaración de la variable respuesta.
-let respuesta = "";
+// Obtenemos el valor del rango del préstamo.
+rangoImporte.addEventListener('mousemove', () => {
+	importe.innerText = rangoImporte.value;
+})
 
-// Bucle que se repite si la respuesta del último prompt es 'si'.
-do {
-	// Declaración de variables.
-  let prestamo = 0;
-  let cuotas = 0;
-  let valorCuota = 0;
-  
-	// Solicitud al usuario de importe de préstamo.
-  prestamo = parseFloat(prompt("Ingrese el importe del préstamo que desea simular."));
+// Manejamos el submit.
+formContainer.addEventListener ('submit', (e) => {
+	// Suprimimos el comportamiento por defecto y mostramos la card con los resultados
+	e.preventDefault();
+	formContainer.style.display = 'none';
+	resultsContainer.style.display = 'flex';
 
-	//Llamada de función de validación.
-	prestamo = validacionNumber(prestamo, "El valor ingresado no es válido. Por favor ingrese el importe del préstamo en valor numérico mayor a 0.");
+	// Obtenemos elementos del formulario del DOM.
+	let email = document.querySelector('.input__form').value;
+	let resultEmail = document.querySelector('.results__span__email')
+	let resultPrestamo = document.querySelector('#prestamo');
+	let resultCuotas = document.querySelector('#cuotas__results');
+	let importeCuota = document.querySelector('#importe-cuota');
+	let fechaVencimiento = document.querySelector('.results__span__fecha')
+	let historialVacio = document.querySelector('.historial__vacio')
 
-	// Solicitud al usuario de cantidad de cuotas.
-  cuotas = parseFloat(prompt("Ingrese la cantidad de cuotas."));
+	// Mostramos el mail en la card de resultados.
+	resultEmail.innerText = email;
 
-	// Llamada de función de validación.
-	cuotas = validacionNumber(cuotas, "El valor ingresado no es válido. Por favor ingrese la cantidad de cuotas en valor numérico mayor a 0.");
+	// Agarramos la fecha del día de hoy, la formateamos y le agregamos un mes más para que la primer cuota se pague dentro de un mes.
+	let diaDeHoy = new Date();
 
-	// Llamada de función de cálculo.
-  valorCuota = calculoIntereses(prestamo, cuotas);
+	let fechaFormateada = diaDeHoy.getDate() + "-" + (diaDeHoy.getMonth() + 2) + "-" + diaDeHoy.getFullYear()
 
-  // Por cada préstamo simulado se crea un objeto
-  class Prestamo {
-	constructor(prestamo, cuotas, valorCuota) {
-		this.prestamo = prestamo;
-		this.cuotas = cuotas;
-		this.valorCuota = valorCuota;
-	}
-  }
+	fechaVencimiento.innerText = fechaFormateada;
 
+	// Mostramos el valor del préstamo simulado.
+	resultPrestamo.innerText = '$' + rangoImporte.value;
 
-  // Insertamos el objeto en el array de prestamos
-  prestamos.push(new Prestamo(prestamo, cuotas, valorCuota));
+	// Una vez que tenemos préstamos en el historial, se borra el mensaje de que no se simularon préstamos.
+	historialVacio.style.display = 'none'
 
-  // Mapeamos los préstamos para obtener el importe de cada uno
-  prestamosActualizados = prestamos.map(prestamo => {
-	return prestamo.prestamo;
-  })
+	// Tomamos el valor seleccionado de las cuotas y lo imprimimos.
+	cuotas.forEach(cuota => {
+		if(cuota.checked) {
+			let valorCuota = calculoIntereses(parseFloat(rangoImporte.value), parseFloat(cuota.value))
+			resultCuotas.innerText = 'a ' + cuota.value + ' meses.'
+			importeCuota.innerText = '$' + valorCuota;
 
-	// Se muestra el resultado final del préstamo.
-	if(cuotas == 1) {
-		alert(`El préstamo de $${prestamo} se cotizará en 1 cuota de $${valorCuota}`);
-	} else {
-		alert(`El préstamo de $${prestamo} se cotizará en ${cuotas} cuotas de $${valorCuota}`);
-	}
+			// Por cada préstamo simulado se crea un objeto
+			class Prestamo {
+				constructor(prestamo, cuotas, valorCuota) {
+					this.prestamo = rangoImporte.value;
+					this.cuotas = cuota.value;
+					this.valorCuota = valorCuota;
+				}
+			}
 
-	// Solicitud al usuario de continuación.
-  respuesta = prompt("Quiere simular otro préstamo?").toLowerCase();
+			// Insertamos el objeto en el array de prestamos.
+			prestamos.push(new Prestamo(prestamo, cuotas, valorCuota));
 
-	// Llamada de función de validación.
-	respuesta = validacionRespuesta(respuesta, "El valor ingresado no es válido. Por favor, si desea continuar ingrese 'si', de lo contrario ingrese 'no'");
+			// Mapeamos los préstamos y le agregamos un ID.
+			let prestamosConId = prestamos.map((prestamo, index) => {
 
-} while (respuesta == "si");
+				return {
+						...prestamo,
+						id: index + 1
+				}
+			})
 
-// Mostramos al usuario el importe de los préstamos solicitados.
-alert("Ha simulado los siguientes préstamos: \n$" + prestamosActualizados.join("\n$") + ". \nNos vemos la próxima!")
+			// Guardamos los préstamos simulados en el localStorage.
+			localStorage.setItem('prestamos', JSON.stringify(prestamosConId))
+
+			// Tomamos los préstamos guardados en el localStorage y los mostramos.
+			let prestamosHistorial = JSON.parse(localStorage.getItem('prestamos'));
+
+			
+			let historialInfo = document.createElement('div');
+
+			historialInfo.classList.add('text__historial')
+			for(let i = 0 ; i <= prestamosHistorial.length; i++) {
+				historialInfo.innerHTML = '<h6>Prestamo ' + prestamosHistorial[i].id + '</h6>'
+				historialInfo.innerHTML += '<p>Importe: ' + prestamosHistorial[i].prestamo + '</p>'
+				historialInfo.innerHTML += '<p>Cantidad de cuotas: ' + prestamosHistorial[i].cuotas + '</p>'
+				historialInfo.innerHTML += '<p>Valor de cuotas: ' + prestamosHistorial[i].valorCuota + '</p>'
+				historialPrestamos.append(historialInfo)
+		}
+		}
+	})
+	
+});
+
+// Si se solicita el préstamo aparece una alerta.
+solicitarButton.addEventListener('click', () => {
+	alerta.classList.add('slideInDown')
+	alerta.style.display = 'flex'
+	alerta.innerHTML = '<img src="./assets/asset-project-coder-check.svg" alt="check">';
+	alerta.innerHTML += '<p>Adquiriste tu préstamo satisfactoriamente!</p>';
+})
+
+// Si se quiere volver a simular aparece la card anterior.
+resetButton.addEventListener('click', () => {
+	formContainer.style.display = 'flex';
+	resultsContainer.style.display = 'none';
+	alerta.style.display = 'none';
+})
+
+// Se muestra y se oculta la sección de historial cuando se hace click en el botón.
+historialButton.addEventListener('click', () => {
+	historialPrestamos.classList.toggle('show')
+})
